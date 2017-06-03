b6495725c338ac4d6daf5af6847e2a40d17af7c4
rieuse <rieuse@163.com>
2017-06-03 15:08:21 +0800

    修改

--- 斗鱼主播大司马弹幕词云/弹幕分析词云.py
+++ 斗鱼主播大司马弹幕词云/弹幕分析词云.py
@@ -10,28 +10,28 @@ import os
 import PIL.Image as Image
 import numpy as np
 
-with open('大司马上课后.txt','r',encoding='utf-8') as f:
+with open('大司马即将上课前后.txt','r',encoding='utf-8') as f:
     text = f.read()
     f.close()
-cut_text = " ".join(jieba.cut(text))
+cut_text = " ".join(jieba.cut(text))  #使用空格连接 进行中文分词
 
-d = os.path.dirname(__file__)
-color_mask = np.array(Image.open(os.path.join(d,'img.jpg')))
-my_wordcloud = WordCloud(
-    background_color='#F0F8FF',      # 背景颜色
-    font_path="FZLTKHK--GBK1-0.ttf", # 使用特殊字体可以显示中文
-    max_words=8000,
-    font_step=20,    # 步调太大，显示的词语就少了
-    mask=color_mask,
+d = os.path.dirname(__file__) # 获取当前文件路径
+color_mask = np.array(Image.open(os.path.join(d,'img.jpg')))   # 设置图片
+cloud = WordCloud(
+    background_color='#F0F8FF',      # 参数为设置背景颜色,默认颜色则为黑色
+    font_path="FZLTKHK--GBK1-0.ttf", # 使用指定字体可以显示中文，或者修改wordcloud.py文件字体设置并且放入相应字体文件
+    max_words=1000,  # 词云显示的最大词数
+    font_step=10,    # 步调太大，显示的词语就少了
+    mask=color_mask,  #设置背景图片
     random_state= 15, # 设置有多少种随机生成状态，即有多少种配色方案
-    min_font_size=15,
-    max_font_size=202,
+    min_font_size=15,  #字体最小值
+    max_font_size=232, #字体最大值
     )
-my_wordcloud.generate(cut_text)
-image_colors = ImageColorGenerator(color_mask)
-plt.show(my_wordcloud.recolor(color_func=image_colors))
-plt.imshow(my_wordcloud)            # 以图片的形式显示词云
+cloud.generate(cut_text)  #对分词后的文本生成词云
+image_colors = ImageColorGenerator(color_mask)  # 从背景图片生成颜色值
+plt.show(cloud.recolor(color_func=image_colors))  # 绘制时用背景图片做为颜色的图片
+plt.imshow(cloud)            # 以图片的形式显示词云
 plt.axis('off')                     # 关闭坐标轴
 plt.show()                          # 展示图片
 
-my_wordcloud.to_file(os.path.join(d, 'pic.jpg'))
+cloud.to_file(os.path.join(d, 'pic.jpg'))  # 图片大小将会按照 mask 保存
